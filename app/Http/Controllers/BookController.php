369ab7afb1ed3812a69452984fc09f787c7b154d
kibet kilel <amos.rotich@strathmore.edu>
2022-03-22 16:35:50 +0300

    Update

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -26,12 +26,29 @@ class BookController extends Controller
     public function store(Request $request)
     {
         //Post data to database from user
+        //Validate the data
+        $this -> validate($request,[
+            'name' => 'required',
+            'author' => 'required',
+            'comment' => 'required'
+        ]);
+        $book = new Book();
+
+        $book->name = $request->input('name');
+        $book->author = $request->input('author');
+        $book->comment = $request->input('comment');
+
+        $book->save();
+        return response()->json($book);
 
     }
 
     public function show($id)
     {
         //give 1 item from books
+        $book = Book::find($id);
+
+        return response() ->json($book);
 
     }
 
@@ -44,11 +61,32 @@ class BookController extends Controller
     public function update(Request $request, $id)
     {
         // update by id
+
+           //Validate the data
+           $this -> validate($request,[
+            'name' => 'required',
+            'author' => 'required',
+            'comment' => 'required'
+        ]);
+        $book = Book::find($id);
+
+        $book->name = $request->input('name');
+        $book->author = $request->input('author');
+        $book->comment = $request->input('comment');
+
+        $book->save();
+        return response()->json($book);
+
     }
 
 
     public function destroy($id)
     {
         // delete different books using id
+
+        $book = Book::find($id);
+        $book->delete();
+
+        return response() ->json('Book deletd successfully');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,17 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+//$verbs = ['GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE'];
+
+//$verbs = ['GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE'];
 $router->get('/books', 'BookControler@index');
+
 $router->get('/books/{id}', 'BookControler@show');
+
 $router->post('/books/create', 'BookControler@store');
+
 $router->put('/books/update/{id}', 'BookControler@update');
+
 $router->delete('/books/delete/{id}', 'BookControler@destroy');
 
 
