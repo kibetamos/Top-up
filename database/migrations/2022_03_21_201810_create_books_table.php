632611f9d2d913e4fa6e8a910b120b6bbfed57b0
kibet kilel <amos.rotich@strathmore.edu>
2022-03-28 09:59:28 +0300

    Database update

--- database/migrations/2022_03_21_201810_create_books_table.php
+++ database/migrations/2022_03_21_201810_create_books_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title')->unique();
             $table->string('author');
             $table->text('description');
             $table->timestamps();
